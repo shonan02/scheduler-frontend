2dbfe6629632e58b87a3a3485b986290215e2856
shonan02 <66680460+shonan02@users.noreply.github.com>
2024-01-02 13:12:08 +0000

    Add facebook login functionality

---
+++ scheduler/src/components/FacebookLoginButton.jsx
@@ -0,0 +1,31 @@
+const FacebookLoginButton = ({ onLogin }) => {
+    const handleLogin = () => {
+        //Initialise Facebook SDK
+        window.FB.init({
+            appId: '339992368896098',
+            autoLogAppEvents: true,
+            xfbml: true,
+            version: 'v14.0'
+        });
+
+        //Trigger login dialogue
+        window.FB.login(res => {
+            if(res.authResponse) {
+                //User is logged in and granted permissions
+                onLogin(res.authResponse.accessToken);
+            } else {
+                //User cancelled login
+            }
+        }, { scope: 'public_profile,email'});
+    }
+
+    return (
+        <>
+        <button onClick={handleLogin}>
+            Login with facebook
+        </button>
+        </>
+    )
+}
+
+export default FacebookLoginButton;
