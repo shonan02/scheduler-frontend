2f57e5f9b0a82f6c106758fbf42338265503e7b1
shonan02 <66680460+shonan02@users.noreply.github.com>
2024-01-03 23:02:58 +0000

    Usereducer

--- scheduler/src/main.jsx
+++ scheduler/src/main.jsx
@@ -3,12 +3,23 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import userReducer from './reducers/userReducer.js';
+
+const store = configureStore({
+  reducer: {
+    user: userReducer
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
---
+++ scheduler/src/reducers/userReducer.js
@@ -0,0 +1,31 @@
+import { createSlice } from "@reduxjs/toolkit";
+import loginService from "../services/login";
+import { useSearchParams } from "react-router-dom";
+
+const userSlice = createSlice({
+    name: "user",
+    initialState: [],
+    reducers: {
+        setUser(state, action) {
+            return action.payload;
+        },
+        removeUser(state, action) {
+            return null;
+        }
+    }
+})
+
+export const loginUser = (user) => {
+    return async dispatch => {
+        try {
+            const loggedUser = await loginService.login(user);
+            window.localStorage.setItem("loggedUser", JSON.stringify(loggedUser));
+            dispatch(setUser(loggedUser));
+        } catch(err) {
+            console.log(err);
+        }
+    }
+}
+
+export const { setUser, deleteUser } = userSlice.actions;
+export default userSlice.reducer;
