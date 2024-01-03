e081be7e926980f7016b9fa2f2bf0755e205d4a4
shonan02 <66680460+shonan02@users.noreply.github.com>
2024-01-03 16:19:13 +0000

    Navbar

--- scheduler/src/App.jsx
+++ scheduler/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   
   return (
     <>
-    {/* <Navigation /> */}
+    <Navigation />
     <FacebookLoginButton onLogin={onLogin} />
     <Reigster />
     <Login />
--- scheduler/src/components/Navigation/Navigation.jsx
+++ scheduler/src/components/Navigation/Navigation.jsx
@@ -1,12 +1,24 @@
 import './Navigation.css';
+import {Link} from 'react-router-dom';
 
 const Navigation = () => {
     
     return (
         <nav className='navbar'>
-            <ul>
-                <li>Log In</li>
-            </ul>
+            <div className='nav-container'>
+                <div className='nav-logo'>
+                    {/* branding */}
+                </div>
+                <div className='nav-elements'>
+                    <ul>
+                        <li>
+                            {/* <Link to="/">Home</Link> */}
+                        </li>
+                        <li>Log in</li>
+                        <li>Register</li>
+                    </ul>
+                </div>
+            </div>
         </nav>
     )
 }
